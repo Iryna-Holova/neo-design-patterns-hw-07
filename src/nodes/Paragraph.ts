9f06f357c290ff7d6861b3a7937f60eebaed21f2
Iryna Holova <gis.golova@gmail.com>
2026-06-16 21:25:09 +0300

    Add start files

---
+++ src/nodes/Paragraph.ts
@@ -0,0 +1,12 @@
+import { DocNode } from '../interfaces/DocNode';
+import { DocRenderer } from '../interfaces/DocRenderer';
+
+export class Paragraph implements DocNode {
+  constructor(
+    private text: string,
+    private renderer: DocRenderer,
+  ) {}
+  render(): string {
+    // TODO: Implement the render method
+  }
+}
